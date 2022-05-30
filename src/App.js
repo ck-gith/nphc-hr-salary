b1d64f08ced6f7f6d4260daf57c2717bfea3cf51
Lee Chun Keat <chunkeat.ck@hotmail.com>
2022-05-30 15:03:55 +0800

    added get api call, table pagination, column sorting,and salary filtering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import 'antd/dist/antd.min.css';
-import { useState } from 'react';
-import { Table } from 'antd';
+import { useEffect, useState } from 'react';
+import { Table, Space, message } from 'antd';
+import axios from 'axios';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+
 
 function App() {
+  const paginationConfig = {
+    pageSizeOptions: ['5', '10', '15']
+  };
   const [dataSource, setdataSource] = useState(generateDataSource(100));
+  // const [dataSource, setdataSource] = useState([]);
 
-  generateDataSource(100);
+  const url = 'https://nphc-hr.free.beeceptor.com/employees';
+  // temporary comment out for testing , free version of beeceptor have limit of 50 per day
+  // useEffect(() => {
+  //   axios
+  //     .get(url)
+  //     .then((response) => {
+  //       console.log(response);
+  //       setdataSource(response.data)
+  //     }
+  //     )
+  //     .catch((error) => message.error('There is an error when getting employee data.', error.message));
+  //   // .finally(() => setLoaded(true));
+  // }, []);
 
   function generateDataSource(value) {
     let dataSourceArray = [];
@@ -14,9 +33,9 @@ function App() {
       dataSourceArray.push({
         key: i,
         id: i,
-        name: 'Harry Potter ' + i,
-        login: 'hpotter',
-        salary: '1234.00',
+        full_name: 'Harry Potter ' + i,
+        login_id: 'hpotter',
+        salary: (i + 1049).toString(),
       });
     };
     return dataSourceArray;
@@ -27,22 +46,36 @@ function App() {
       title: 'ID',
       dataIndex: 'id',
       key: 'id',
+      sorter: (a, b) => a.id - b.id,
     },
     {
       title: 'Name',
-      dataIndex: 'name',
+      dataIndex: 'full_name',
       key: 'name',
+      sorter: (a, b) => a.full_name.localeCompare(b.full_name),
     },
     {
       title: 'Login',
-      dataIndex: 'login',
+      dataIndex: 'login_id',
       key: 'login',
+      sorter: (a, b) => a.login_id.localeCompare(b.login_id),
     },
     {
       title: 'Salary',
       dataIndex: 'salary',
       key: 'salary',
-    }
+      sorter: (a, b) => a.salary - b.salary,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (record) => (
+        <Space>
+          <EditOutlined></EditOutlined>
+          <DeleteOutlined></DeleteOutlined>
+        </Space>
+      ),
+    },
   ];
 
   return (
@@ -50,6 +83,7 @@ function App() {
       <Table
         dataSource={dataSource}
         columns={columns}
+        pagination={paginationConfig}
       />
     </div>
   );
